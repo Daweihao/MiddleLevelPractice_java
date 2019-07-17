7533d48db4bd7adeedede5e4a5db5742c567a8f2
Daweihao <421679009@qq.com>
2019-07-17 18:00:19 +0800

    filereader

--- src/io/TestStream.java
+++ src/io/TestStream.java
@@ -5,27 +5,39 @@ import java.io.*;
 public class TestStream {
     public static void main(String[] args) {
 
-        try {
-            File f = new File("Readme.md");
-            FileInputStream fis = new FileInputStream(f);
-            File f1 = new File("xyz/pyq/Test.txt");
-            File parent  = f1.getParentFile();
-            if(!parent.exists()){
-                parent.mkdirs();
+//        try {
+//            File f = new File("Readme.md");
+//            FileInputStream fis = new FileInputStream(f);
+//            File f1 = new File("xyz/pyq/Test.txt");
+//            File parent  = f1.getParentFile();
+//            if(!parent.exists()){
+//                parent.mkdirs();
+//            }
+//
+//            byte[] all = new byte[((int) f.length())];
+//            fis.read(all);
+//            for (byte each : all){
+//                System.out.println(each);
+//            }
+//            fis.close();
+//
+//            FileOutputStream fos = new FileOutputStream(f1);
+//            byte data[] = {88,89};
+//            fos.write(data);
+//            fos.close();
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        File f = new File("Readme.md");
+        try (FileReader fr = new FileReader(f)){
+            char[] all = new char[(int) f.length()];
+            fr.read(all);
+            for (char b : all){
+                System.out.println(b);
             }
-
-            byte[] all = new byte[((int) f.length())];
-            fis.read(all);
-            for (byte each : all){
-                System.out.println(each);
-            }
-            fis.close();
-
-            FileOutputStream fos = new FileOutputStream(f1);
-            byte data[] = {88,89};
-            fos.write(data);
-            fos.close();
-
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
